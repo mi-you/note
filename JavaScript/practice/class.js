e9c4534352f922d1514fb7ed83cd39559452daf3
米又 <15755276427@163.com>
2021-09-28 21:40:08 +0800

    fix: 移除javascript到新仓库

--- JavaScript/practice/class.js
+++
@@ -1,7 +0,0 @@
-let Person = class { 
-  identify() { 
-    console.log(Person.name); 
-  } 
-} 
-let p = new Person(); 
-p.identify(); // PersonName PersonName
--- JavaScript/practice/worker/globalScopeWorker.js
+++
@@ -1,6 +0,0 @@
-// self.addEventListener('message',({data}) => console.log(data))
-
-// self.postMessage('self1')
-// self.close()
-// self.postMessage('self2')
-// setTimeout(() => self.postMessage('self3'),0)
